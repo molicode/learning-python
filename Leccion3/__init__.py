f382de85d1ddf60138e8557697e2a499f27faa22
Luis Molina <molicode1347@gmail.com>
2022-12-13 18:49:33 -0300

    Initial commit

---
+++ Leccion3/__init__.py
@@ -0,0 +1,17 @@
+# Python data types
+
+x = 10
+print(x)
+print(type(x))
+
+x: str = "Hola Mundo"
+print(x)
+print(type(x))
+
+x = 2.3
+print(x)
+print(type(x))
+
+x = True
+print(x)
+print(type(x))
