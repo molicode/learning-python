09a5e26d24d2c97810cb4a541abb6f1048996a19
Luis Molina <molicode1347@gmail.com>
2022-12-13 18:39:28 -0300

    Initial commit

---
+++ Leccion02/__init__.py
@@ -0,0 +1,9 @@
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
+
+# la funcción id permite conocer en que posición de memoría está almancenada la varaiable x
+print(id(x))
---
+++ Leccion02/main.py
@@ -0,0 +1,8 @@
+# declaracion de variables
+nombre = 'Juan Perez'
+telefono = 55618867
+email = 'jperez@perez.com'
+
+print(nombre)
+print(telefono)
+print(email)
